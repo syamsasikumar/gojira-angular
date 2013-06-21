a0d39f61b839717a489f7a6c482dbe230aef0b57
syam sasikumar <syamsasikumar@gmail.com>
2013-06-20 20:45:31 -0400

    build file

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -1,9 +1,10 @@
-define(['backbone'], function(Backbone) {
+define(['backbone', 'jquery'], function(Backbone, $) {
   var App = Backbone.View.extend({
     initialize: function() {
-      console.log( 'In View' );
+     // console.log( 'In View' );
+      console.log("test="+ $('#test').html());
     }
   });
-
+  
   return App;
 });
