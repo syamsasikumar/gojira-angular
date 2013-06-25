eaa1dc2895a3f4777e9d948921c668f21010d16e
syam sasikumar <syamsasikumar@gmail.com>
2013-06-24 21:50:21 -0400

    login changed to collapsable

--- src/app/user/user.js
+++ src/app/user/user.js
@@ -5,12 +5,17 @@ angular.module( 'gojira.user', [
   'Auth'
 ])
 .controller( 'UserCtrl', function UserCtrl ( $scope, AuthService ) {
+	$scope.isCollapsed = true;
 	$scope.login = function(){
 		console.log('Clicked login');
 	};
 	$scope.register = function(){
 		console.log('Clicked register');
 	};	
+	$scope.toggleUser = function(){
+		$scope.isCollapsed = !$scope.isCollapsed;
+		console.log('here');
+	}
 	$scope.$watch( AuthService.isLoggedIn, function(isLoggedIn){
 		$scope.user = AuthService.getUser();
 		$scope.userContent = (isLoggedIn)? "user/user.tpl.html":"user/anon.tpl.html";
